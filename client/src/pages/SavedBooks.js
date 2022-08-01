653ed3292b333161d667e345c0bb01f957cb2b2a
jonghwa101 <97046027+jonghwa101@users.noreply.github.com>
2022-08-01 03:14:50 -0400

    updated pages

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -10,7 +10,7 @@ import Auth from '../utils/auth';
 
 const SavedBooks = () => {
   const { loading, data } = useQuery(QUERY_ME);
-  const [removeBook, { error }] = useMutation(REMOVE_BOOK);
+  const [removeBook] = useMutation(REMOVE_BOOK);
 
   const userData = data?.me || {};
 
@@ -23,7 +23,7 @@ const SavedBooks = () => {
     }
 
     try {
-      const { data } = await removeBook({
+      await removeBook({
         variables: { bookId },
       });
 
